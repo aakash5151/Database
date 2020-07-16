3badf90be6935688f3d56f2ea80ffe3fdd823904
aakash5151 <54582953+aakash5151@users.noreply.github.com>
2020-07-17 00:02:39 +0530

    Update SQL_Asgn1.sql

--- Day 2 Assgn/SQL_Asgn1.sql
+++ Day 2 Assgn/SQL_Asgn1.sql
@@ -47,9 +47,8 @@ insert into orders values(3002,1900.10,'1990-10-03',2007,1004);
 insert into orders values(3005,5160.45,'1990-10-03',2003,1002);
 insert into orders values(3006,1098.16,'1990-10-03',2008,1007);
 insert into orders values(3009,1713.23,'1990-10-04',2002,1003);
-insert into orders values(3007,75.75,'1990-10-04',2004,100,1002);
+insert into orders values(3007,75.75,'1990-10-04',2004,1002);
 insert into orders values(3008,4723.00,'1990-10-05',2006,1001);
-insert into orders values(3010,1309.95,'1990-10-06',2004,1002);
 insert into orders values(3010,1309.95,'1990-10-06',2004,1002);
 insert into orders values(3011,9891.88,'1990-10-06',2006,1001);
 
@@ -59,4 +58,3 @@ select * from customers;
 
 delete from salespeople where snum=1001;
 
-select city CITY,comm "COMM" from salespeople where comm >0.11;
