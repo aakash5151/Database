f837bc6470c50c2c90849b3f97309f3ea654f9c6
aakash5151 <54582953+aakash5151@users.noreply.github.com>
2020-07-23 23:54:38 +0530

    updated

--- Day 5/Exercise 3.sql
+++ Day 5/Exercise 3.sql
@@ -19,8 +19,56 @@ select sname,length(sname) from s;
  
  select * from s where soundex(sname)=soundex('bobbbby');
  
+ select sname,case
+    when status=10 then 'ten'
+    when status=20 then 'twenty'
+    when status=30 then 'thirty'
+    when status=40 then 'forty'
+    when status=50 then 'fifty'
+    when status=70 then 'seventy'
+    else 'others'
+    END status from s;
+ 
  select dayname(curdate());
  
  select curdate();
+ 
+ 
+ ----------------------------------------
+ ---practice---
+ create table dept(
+ 
+ dno int(4),
+ dname varchar(20),
+loc varchar(20)
+ );
+ 
+ 
+ insert into dept values(10,'sales','mumbai'),
+						(20,'training','delhi'),
+						(30,'research','pune');
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  
